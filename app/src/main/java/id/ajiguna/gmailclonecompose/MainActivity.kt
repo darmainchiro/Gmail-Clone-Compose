bf645c45c5dbeb907a2910e5d609d394454e82ec
Aji Guna <gunaitsugo@gmail.com>
2023-08-21 23:12:38 +0700

    navigation drawer

--- app/src/main/java/id/ajiguna/gmailclonecompose/MainActivity.kt
+++ app/src/main/java/id/ajiguna/gmailclonecompose/MainActivity.kt
@@ -1,15 +1,21 @@
 package id.ajiguna.gmailclonecompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import id.ajiguna.gmailclonecompose.components.GmailDrawerMenu
+import id.ajiguna.gmailclonecompose.components.HomeAppBar
 import id.ajiguna.gmailclonecompose.ui.theme.GmailCloneComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,27 +26,37 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    GmailApp()
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GmailApp() {
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+    val scrollState = rememberScrollState()
+
+    Scaffold(scaffoldState = scaffoldState,
+        topBar = {
+            HomeAppBar(scaffoldState,coroutineScope)
+        },
+        drawerContent = {
+            GmailDrawerMenu(scrollState = scrollState)
+        }){}
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GmailCloneComposeTheme {
-        Greeting("Android")
+        GmailApp()
     }
 }
---
+++ app/src/main/java/id/ajiguna/gmailclonecompose/components/HomeAppBar.kt
@@ -0,0 +1,71 @@
+package id.ajiguna.gmailclonecompose.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.ajiguna.gmailclonecompose.GmailApp
+import id.ajiguna.gmailclonecompose.R
+import id.ajiguna.gmailclonecompose.ui.theme.GmailCloneComposeTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun HomeAppBar(scaffoldState: ScaffoldState, scope: CoroutineScope) {
+    Box(modifier = Modifier.padding(10.dp)) {
+        Card (modifier = Modifier.requiredHeight(50.dp),
+            shape = RoundedCornerShape(10.dp),
+            elevation = 6.dp){
+                Row(verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp)
+                ){
+                    IconButton(onClick = {
+                        scope.launch { scaffoldState.drawerState.open() }
+                    }) {
+                        Icon(Icons.Default.Menu , contentDescription ="Menu" )
+                    }
+                    Text(text = "Search in emails", modifier = Modifier.weight(2.0f))
+                    Image(painter = painterResource(id = R.drawable.inchiro),
+                        contentDescription = "Profile",
+                        modifier = Modifier
+                            .size(30.dp)
+                            .clip(CircleShape)
+                            .background(color = Color.Gray)
+                    )
+                }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun DefaultPreview(){
+    GmailCloneComposeTheme {
+        GmailApp()
+    }
+}
